12214b3fe17608b8a239d2d3def9595f087348cc
Anup Kumar Panwar <1anuppanwar@gmail.com>
2018-11-29 21:23:11 +0530

    Remove Image

---
+++ admin/removeImage.php
@@ -0,0 +1,44 @@
+<?php
+
+require ('constants.php');
+require ('middleware.php');
+
+
+if (isset($_POST['id'])) {
+
+	$image_id = mysqli_escape_string($conn, $_POST['id']);
+
+	$removeImageQuery = "DELETE FROM store_pictures WHERE id='$image_id'";
+
+	$result = mysqli_query($conn, $removeImageQuery);
+
+	if ($result) {
+		$response = array(
+		    'result' => array(
+		        'success' => True,
+		        'message' => 'Image removed successfully.'
+		    )
+		);
+		die(json_encode($response));
+	}
+	else {
+		$response = array(
+		    'result' => array(
+		        'success' => False,
+		        'message' => 'Failed to remove image.'
+		    )
+		);
+		die(json_encode($response));
+	}
+}
+else
+{
+    $response = array(
+        'result' => array(
+            'success' => False,
+            'message' => 'Some error occured.'
+        )
+    );
+    die(json_encode($response));
+}
+?>
