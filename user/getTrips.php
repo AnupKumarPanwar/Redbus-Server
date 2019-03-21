1278101662cf326223546a1a7798b78b02649f4b
Anup Kumar Panwar <1anuppanwar@gmail.com>
2019-03-21 09:24:19 +0530

    get trips

---
+++ user/getTrips.php
@@ -0,0 +1,35 @@
+<?php
+
+include_once ('constants.php');
+include_once ('middleware.php');
+
+$user_id = $_SESSION['user_id'];
+$getTrips = "SELECT * FROM bookings, trips, buses WHERE bookings.trip_id = trips.id AND trips.bus_id=buses.id AND bookings.user_id='$user_id'";
+
+$result = mysqli_query($conn, $getTrips);
+
+if ($result) {
+	
+		$r = mysqli_fetch_assoc($result);
+
+		$response = array(
+		    'result' => array(
+		        'success' => True,
+		        'message' => 'Trips fetched successfully.',
+		        'data' => $r
+		    )
+		);
+		die(sendResponse($response));
+	
+}
+else {
+	$response = array(
+	    'result' => array(
+	        'success' => False,
+	        'message' => 'Failed to get trips.'
+	    )
+	);
+	die(sendResponse($response));
+}
+
+?>
